157becede2ae9b5b776fa19802c948100da5bfe8
Owain <owain.jones74@gmail.com>
2020-11-27 14:35:19 +0000

    sql basis

---
+++ config.lua
@@ -0,0 +1,8 @@
+local CONFIG = {}
+
+-- The bot's token
+CONFIG.token = "NzgxODg0NzM2Njc0MzMyNjk0.X8EI5w.wt6ZiZqO5kswubxZrilttl8L5Vg"
+-- Should the command handler automatically handle the running of commands?
+CONFIG.commandEvent = true
+
+return CONFIG 
